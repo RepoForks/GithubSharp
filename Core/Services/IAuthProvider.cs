c98d3b665e89705c4ec204489e0f53510f394809
Erik Zaadi <erik.zaadi@gmail.com>
2011-09-29 16:37:46 +0300

    started thinking about oath and basic auth

---
+++ Core/Services/IAuthProvider.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace GithubSharp.Core.Services
+{
+    public interface IAuthProvider
+    {
+        IAuthResponse Login(Models.GithubUser githubUser);
+        IAuthResponse Logout();
+        IAuthResponseWithContent<string> ModifyAPIUrl(string apiUrl);
+    }
+
+    public interface IAuthResponse
+    {
+        string Message { get; set; }
+        bool Success { get; set; }
+    }
+
+    public interface IAuthResponseWithContent<TContentType> : IAuthResponse
+    {
+        TContentType Content { get; set; }
+    }
+
+    public class AuthResponse : IAuthResponse
+    {
+        public string Message { get; set; }
+        public bool Success { get; set; }
+    }
+
+    public class AuthResponseWithContent<TContentType> : AuthResponse, IAuthResponseWithContent<TContentType>
+    {
+        public TContentType Content { get; set; }
+    }
+}
---
+++ Plugins/AuthProviders/BasicAuthProvider/BasicAuthProvider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GithubSharp.Core.Services;
+
+namespace GithubSharp.Plugins.AuthProviders.BasicAuthProvider
+{
+    public class BasicAuthProvider : IAuthProvider
+    {
+        public IAuthResponse Login(GithubSharp.Core.Models.GithubUser githubUser)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAuthResponse Logout()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAuthResponseWithContent<string> ModifyAPIUrl(string apiUrl)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
